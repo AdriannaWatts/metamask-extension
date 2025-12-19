909bbbc0717c154c6c2d0fe9d99d871daac436c6
Micaela Estabillo <micaela.estabillo@consensys.net>
2025-12-19 08:58:15 -0800

    fix: eth token assetId

--- ui/pages/bridge/prepare/prepare-bridge-page.test.tsx
+++ ui/pages/bridge/prepare/prepare-bridge-page.test.tsx
@@ -125,6 +125,7 @@ describe('PrepareBridgePage', () => {
           symbol: 'UNI',
           address: '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
           decimals: 6,
+          chainId: CHAIN_IDS.LINEA_MAINNET,
           assetId:
             'eip155:59144/erc20:0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
         },
